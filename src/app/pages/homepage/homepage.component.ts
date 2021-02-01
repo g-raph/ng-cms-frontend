5372e8580b65eeee33020242dbd6330b23ff1508
Gielen Gunter <Gunter.Gielen@cegeka.com>
2021-02-01 15:40:48 +0100

    Restaurant teaser on homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/api.service';
+import { Category } from 'src/app/categories/category';
+import { Restaurant } from 'src/app/restaurants/restaurant';
 import { Homepage } from './homepage';
 
 @Component({
@@ -11,11 +13,15 @@ import { Homepage } from './homepage';
 export class HomepageComponent implements OnInit {
 
   homepage$: Observable<Homepage>;
+  restaurants$: Observable<Restaurant[]>;
+  categories$: Observable<Category[]>;
 
   constructor(private api: ApiService) { }
 
   ngOnInit() {
     this.homepage$ = this.api.getHomePage();
+    this.restaurants$ = this.api.getRestaurants();
+    this.categories$ = this.api.getCategories();
   }
 
 }
